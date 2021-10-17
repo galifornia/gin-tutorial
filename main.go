435a59c9ccfcca1159c27fd844bd9d8234186fe2
galifornia <martinho87@gmail.com>
2021-10-18 01:03:44 +0200

    Minimal Album api with 3 endpoints

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -13,6 +19,42 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func main() {
+// getAlbums responds with the list of all albums as JSON
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
+// postAlbums adds an album from JSON received in the request body
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON((&newAlbum)); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
 }
